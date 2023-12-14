2853794f21b5a374d3be4e40b137b0b0aa4b723f
gonzalo freddi <freddigonzalo17@gmail.com>
2023-12-14 11:04:30 -0300

    clear code Api/src/exerciseController

--- Api/src/controllers/exerciseController.js
+++ Api/src/controllers/exerciseController.js
@@ -1,47 +1,32 @@
-import { Exercise, Muscle} from '../db'
+import { Exercise} from '../db'
 import { Op } from 'sequelize'
 import { getMuscleByName } from './MuscleCotrollers'
 
 export const postExerciseController = async (name, image, description, muscles) => {
     try {
-        
         if (muscles) {
             const newExercise = await Exercise.create({ name, image, description })
-            const muscleAdds = await muscles.forEach(async element => {
+            await muscles.forEach(async element => {
                 let muscleName = await getMuscleByName(element)
                 await newExercise.addMuscle(muscleName)
-                console.log('log de los musculos names ', muscleName)
             })
-            console.log('log de los musculos',muscleAdds)
-            return newExercise
+            return 'Exercise created succesfully'
+        }else{
+            return 'must add at least one muscle'
         }
-        
     } catch (error) {
-        console.log('error en el controlador ' , error)
-        return `error in the controller ${error.message}`
-    }    
-    
-
+        return error.message
+    }
 }
-
-
 export const getExerciseByNameController = async(name) => {
-    
     const result = await Exercise.findAll({
         where: {
             name: {
                 [Op.iLike]: `%${name}%`
             },
         },
-       
     })
     return result
-    
 }
 
-export const getAllExerciseController = async()=> {
-
-    const result = await Exercise.findAll()
-    return result
-
-}
+export const getAllExerciseController = async ()=> await Exercise.findAll()
